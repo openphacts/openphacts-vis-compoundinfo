48dfba1af78790adb74bfc695599aee2dfd3c12d
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2015-05-18 13:23:36 +0100

    change ops api to 1.5

--- snippets/simple_example.js
+++ snippets/simple_example.js
@@ -3,7 +3,7 @@ var app = require("openphacts-vis-compoundinfo");
 var instance = new app({
     appID: '949a7c9c',
     appKey: '734a274b418b0dbe57fc40f86e85e20e',
-    appURL: 'https://beta.openphacts.org/1.3',
+    appURL: 'https://beta.openphacts.org/1.5',
     URI: 'http://ops.rsc.org/OPS403534',
     target: yourDiv.id
  });
